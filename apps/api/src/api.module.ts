8389f47c53541466fbace96d9e5c24d9b6703595
yoonhyeongjoon <boys1541@autoa2z.co.kr>
2023-05-18 18:42:09 +0900

    fix:config update

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { VehicleService } from './service/vehicle.service';
+import { VehicleService } from './vehicle/vehicle.service';
 
 @Module({
   imports: [],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [ApiService, VehicleService],
 })
 export class ApiModule {}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(GatewayModule);
+  const configService = app.get(ConfigService);
+  console.log(configService.get('REDIS_HOST'), '여기');
   await app.listen();
 }
 bootstrap();
