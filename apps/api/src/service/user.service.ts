4cb74e313bca0c2fa6f660891d65563e7715c089
yhj0121 <boys1541@naver.com>
2023-08-25 14:37:34 +0900

    test

--- apps/api/src/service/user.service.ts
+++ apps/api/src/service/user.service.ts
@@ -1,19 +1,18 @@
 import { cryptoInput, signupInput } from './../dto/user.dto';
-import { User } from './../../../../libs/persistence/src/entities/user.entity';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as crypto from 'crypto';
-
+import { User } from '@app/entity/entities/user.entity';
 @Injectable()
 export class UserService {
-  static SALTTYPE: cryptoInput;
+  static SALTTYPE = 'sadf';
 
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
     @Inject(UserService.SALTTYPE)
-    private readonly SALTTYPE
+    private readonly SALTTYPE: any
   ) {}
 
   async getEmailAddress(userid): Promise<string> {
@@ -35,11 +34,12 @@ export class UserService {
     const { password, ...without } = signupInput;
     const withoutPassword = { ...without };
 
-    const salt = crypto.randomBytes(this.SALTTYPE.salt);
+    const salt = crypto.randomBytes(this.SALTTYPE.length);
     const hash = crypto.pbkdf2Sync(password, salt.toString('base64'), 100, 64, 'sha512').toString('base64');
+    return true;
   }
 
-  async changePassword(password: string) {
-    //인증 수단 필요
-  }
+  // async changePassword(password: string) {
+  //   //인증 수단 필요
+  // }
 }
